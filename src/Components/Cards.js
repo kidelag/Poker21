11f6d64bed327b48ecbd70e1b4acfd8831b90308
TheoRacherR <theo.racher-raulin@outlook.com>
2021-12-17 14:22:37 +0100

    Theo's part

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,32 +1,54 @@
-const idDeck = "";
+let idDeck;
+let arrCard = [];
+let valueCard = 0;
+let valueAllCards = 0;
 
 
-const Cards = async (cardNumber) => {
-	return displayCards(cardNumber);
-};
+// const Cards = async (cardNumber) => {
+// 	return displayCards(cardNumber);
+// };
 
 const initDeck = async () => {
 	const deckFetch = await fetch(
 	  "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
-	);
-  
-	return await deckFetch.json();
+	).then(response => response.json())
+  idDeck = deckFetch;
+	return await deckFetch.deck_id;
 };
 
-const drawCard = async (number) => {
 
-    const deckID = await initDeck();
+const drawCard = async (deckID) => {
+
+    // const deckID = await initDeck();
 
     const cardsFetch = await fetch(
-        "https://deckofcardsapi.com/api/deck/" + deckID.deck_id + "/draw/?count=" + number
-    );
-    return await cardsFetch.json();
+        "https://deckofcardsapi.com/api/deck/" + deckID + "/draw/?count=1"
+    ).then(response => response.json())
+
+    arrCard = cardsFetch;
+
+    if(arrCard.cards[0].value == "JACK" || arrCard.cards[0].value == "QUEEN" || arrCard.cards[0].value == "KING"){
+      valueCard = 10;
+    }
+    else if(arrCard.cards[0].value == "ACE"){
+      valueCard = 0;
+    }
+    else{
+      valueCard = arrCard.cards[0].value;
+    };
+    
+
+    valueAllCards += parseInt(valueCard);
+
+    return await cardsFetch;
 
 }
 
-const displayCards = async (number) => {
 
-    const cardArray = await drawCard(number);
+
+const displayCards = async () => {
+
+    const cardArray = await drawCard();
 
     const content = cardArray.cards.map((item, key) => (
         `<img src="${item.image}"/>`
--- src/index.js
+++ src/index.js
@@ -1,14 +1,63 @@
-const render = (elem) => {
-  const prevHtml = document.getElementById("app").innerHTML;
-  document.getElementById("app").innerHTML = prevHtml + elem;
-};
+// const render = (elem) => {
+//   const prevHtml = document.getElementById("app").innerHTML;
+//   document.getElementById("app").innerHTML = prevHtml + elem;
+// };
 
-const App = async () => {
-  render(
-    `
-      ${await Cards(3)}
-    `
-  );
-};
+// const App = async () => {
+//   render(
+//     `
+//       ${await Cards(3)}
+//     `
+//   );
+// };
 
-App();
+// // App();
+
+let end = false;
+
+
+document.getElementById("initDeck").addEventListener("click", function() {
+  document.getElementById("state").innerHTML = "Partie en cours"
+  console.log(initDeck());
+  document.getElementById("showIdDeck").style.display = "inline-block";
+  document.getElementById("stopTheGame").style.display = "inline-block";
+})
+
+document.getElementById("showIdDeck").addEventListener("click", function() {
+
+  if(valueAllCards > 21){
+    end = true;
+    document.getElementById("state").innerHTML = "C'est perdu"
+  }
+
+  if(!end){
+  console.log(idDeck.deck_id);
+  console.log(drawCard(idDeck.deck_id));
+  console.log(arrCard.cards[0].suit);
+  document.getElementById("value").innerHTML = "<div>" + valueAllCards + "</div>";
+  document.getElementById("setup").innerHTML += "<div id='uniteCard'>" + 
+    "<div id='textCard'>" + arrCard.cards[0].suit + " and " + arrCard.cards[0].value + "</div>" + 
+    "<img id='imgCard' src=" + arrCard.cards[0].image + ">" +
+  "</div>";
+  }
+
+  
+})
+
+document.getElementById("stopTheGame").addEventListener("click", function() {
+  if(!end){
+      end = true;
+    if(valueAllCards === 21){
+      document.getElementById("state").innerHTML = "C'est gagné"
+    }
+    else if(valueAllCards < 21) {
+      drawCard(idDeck.deck_id)
+      if(valueAllCards > 21){
+        document.getElementById("state").innerHTML = "C'est gagné";
+      }
+      else{
+        document.getElementById("state").innerHTML = "C'est perdu"
+      }
+    }
+  }
+})
