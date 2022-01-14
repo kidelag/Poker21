c8755821f6ffa68991b0ea6c0cda9f8cb700873b
TheoRacherR <theo.racher-raulin@outlook.com>
2022-01-14 21:47:10 +0100

    +connection +cookies

--- src/Script/drawCard.js
+++ src/Script/drawCard.js
@@ -1,59 +1,6 @@
-//fonction pour tirer une carte
-const drawCard = async (deckID) => {
-    try {
-        randomNumber = Math.floor(Math.random() * 50) -25;
-        const cardsFetch = await fetch(
-            "https://deckofcardsapi.com/api/deck/" + deckID + "/draw/?count=1"
-        ).then(response => response.json())
-        
-        deck.remaining = cardsFetch.remaining;
-        arrCard = cardsFetch;
-        let textValueCard = "";            
-    
-        convertValue();
-        
-        valueAllCards += parseInt(valueCard);
-    
-        document.getElementById("value").innerHTML = "Value: " + valueAllCards;
-    
-        document.getElementById("cardLeft").innerHTML = "Number of cards remaining: " + arrCard.remaining;
-    
-        document.getElementById("setup").innerHTML += "<img id='imgCard" + deck.remaining + "' src=" + arrCard.cards[0].image + ">";
-        
-        document.getElementById("imgCard" + deck.remaining).animate([
-            { transform: 'translate(-100%,0%) rotate(0deg)'},
-            {transform: 'translate(0%,0%) rotate(' + randomNumber + 'deg)'}
-        ],
-        {duration: 500})
-    
-        document.getElementById("imgCard" + deck.remaining).style.transform = "rotate(" + randomNumber + "deg)";
-        document.getElementById("imgCard" + deck.remaining).style.position = "absolute";
-        console.log(textValueCard);
-        document.getElementById("histo").innerHTML += "<div id='textCard'>Card: " + arrCard.cards[0].suit + ", value: " + arrCard.cards[0].value + textValueCard + "</div>"
-    
-        if(valueAllCards >= 21){
-            end = true;
-            stopTheGame(false);
-        }
-        // else if(valueAllCards === 21){
-        //     end = true;
-        //     document.getElementById("state").innerHTML = "C'est gagné";
-        // }
-    
-        idCard++;
-        // return await cardsFetch;
-    
-    }
-    catch (error) {
-      console.log(error);
-    }
-  
-  }
-
-  const convertValue = async () => {
+//affiche les valeurs des têtes/Ace
+const convertValue = async () => {
     try{
-        console.log("test")
-        console.log(arrCard.cards[0].value);
         if(arrCard.cards[0].value == "JACK" || arrCard.cards[0].value == "QUEEN" || arrCard.cards[0].value == "KING"){
             valueCard = 10;
             textValueCard = " (" + valueCard + ")";
@@ -66,7 +13,101 @@ const drawCard = async (deckID) => {
             valueCard = arrCard.cards[0].value;
         };
     }
-    catch (err){
-        console.log(err);
+    catch (error){
+        document.getElementById("errorCanvas").style.display = "block";
+        console.log(error);
+        document.getElementById("textError").innerHTML += `<div id="errorMessages">${error}</div>`;
     }
+  }
+
+//fonction pour tirer un nombre de carte défini
+const drawCard = async (deckID, number) => {
+    try {
+
+        for (let numb = 0; numb < number; numb++) {
+
+            randomNumber = Math.floor(Math.random() * 50) -25;
+
+
+            let cardsFetch = await fetch(
+                `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=${number}`
+            ).then(response => response.json())
+
+            let translateStartA = -100;
+            let translateStartO = 0;
+            let translateEndA = 0;
+            let translateEndO = 0;
+            let rotateStart = 0;
+
+            // console.log("cardFetch: " + JSON.stringify(cardsFetch));
+            deck.remaining = cardsFetch.remaining;
+            localStorage.setItem('deckArr', JSON.stringify(deck));
+
+
+            arrCard = cardsFetch;
+
+            arrayOfCard = arrCard;
+            arrayOfCard.randomNumber = randomNumber;
+            arrayOfCard.deckRemaining = deck.remaining;
+            // arrayOfCard.translateStartA = translateStartA;
+            // arrayOfCard.translateStartO = translateStartO;
+            arrayOfCard.translateEndA = translateEndA;
+            arrayOfCard.translateEndO = translateEndO;
+            // arrayOfCard.rotateStart = rotateStart;
+
+            let stored = JSON.parse(localStorage.getItem('arrCards'));
+            stored.push(arrCard);
+            localStorage.setItem('arrCards', JSON.stringify(stored));
+
+
+            let textValueCard = "";
+        
+            convertValue();
+            
+            valueAllCards += parseInt(valueCard);
+            
+        
+            document.getElementById("inputDraw").max = deck.remaining;
+            document.getElementById("value").innerHTML = "Value: " + valueAllCards;
+            document.getElementById("cardLeft").innerHTML = "Number of cards remaining: " + arrCard.remaining;
+            document.getElementById("setup").innerHTML += "<img id='imgCard" + deck.remaining + "' src=" + arrCard.cards[0].image + ">";
+            document.getElementById("imgCard" + deck.remaining).animate([
+                {transform: `translate(${translateStartA}%,${translateStartO}%) rotate(${rotateStart}deg)`},
+                {transform: `translate(${translateEndA}%,${translateEndO}%) rotate(${randomNumber}deg)`}
+            ],
+            {duration: 500})
+            document.getElementById("imgCard" + deck.remaining).style.position = 'absolute';
+
+            // localStorage.setItem('arrCardDeg', )
+            
+            document.getElementById("imgCard" + deck.remaining).style.transform = "rotate(" + randomNumber + "deg)";
+            document.getElementById("imgCard" + deck.remaining).style.position = "absolute"; 
+            document.getElementById("histo").innerHTML += "<div id='textCard'>Card: " + arrCard.cards[0].suit + ", value: " + arrCard.cards[0].value + textValueCard + "</div>"
+            if(valueAllCards >= 21){
+                end = true;
+                stopTheGame(false);
+            }
+            // else if(valueAllCards === 21){
+            //     end = true;
+            //     document.getElementById("state").innerHTML = "C'est gagné";
+            // }
+            // localStorage.setItem('arrLastCardDrawn', arrCard);
+            localStorage.setItem('vallueCardAll', valueAllCards);
+            localStorage.setItem('ended', end);
+
+            document.getElementById("connectNetwork").innerHTML = `Connected to the Network: ${window.navigator.onLine}`
+            document.getElementById("connectAPI").innerHTML = `Connected to the API: ${arrCard.success}`
+      
+            
+            idCard++;
+        }
+        // return await cardsFetch;
+    
+    }
+    catch (error){
+        document.getElementById("errorCanvas").style.display = "block";
+        console.log(error);
+        document.getElementById("textError").innerHTML += `<div id="errorMessages">${error}</div>`;
+    }
+  
   }
--- src/Script/stopTheGame.js
+++ src/Script/stopTheGame.js
@@ -1,43 +1,89 @@
 const stopTheGame = async (bool) => {
 
     try {
+
         document.getElementById("endgame").style.display = "block";
 
-        //si bool = true => on a appuyé sur le bouton "Stop the game"
-        //sinon c'est que la funct a été lancé dans la funct "drawCard"
         if(bool){
-            await drawCard(deck.deck_id);
-            document.getElementById("finalCard").innerHTML = "<div>La dernière carte est le " + arrCard.cards[0].value + " de " + arrCard.cards[0].suit + "</div>";
+            await drawCard(deck.deck_id,1);
+            document.getElementById("finalCard").innerHTML = "<div>The last card drawn is the " + arrCard.cards[0].value + " of " + arrCard.cards[0].suit + "</div>";
             if(valueAllCards > 21){
-                document.getElementById("state").innerHTML = "C'est gagné";
-                document.getElementById("canvas").innerHTML = "<p>C'est gagné</p>"
+
+                // animation(150,100,0,0);
+
+                document.getElementById("state").innerHTML = "We have a winner";
+                document.getElementById("canvas").innerHTML = `
+                    <h1>We have a winner</h1> 
+                    <p>The total of points have been over 21 with ${valueAllCards} points after you've stop the game</p>
+                    <p><div>The last card drawn is the ${arrCard.cards[0].value} of ${arrCard.cards[0].suit}</p>
+                    <img id="imgCard + ${deck.remaining}" src="${arrCard.cards[0].image}">
+                `
                 console.log("gagné");
             }
             else{
-                document.getElementById("state").innerHTML = "C'est perdu";
-                document.getElementById("canvas").innerHTML = "<p>C'est perdu</p>"
+
+                // animation(0,0,150,100);
+
+                document.getElementById("state").innerHTML = "It's lost";
+                document.getElementById("canvas").innerHTML = `
+                <h1>It's lost</h1> 
+                <p>The total of points have not been over 21 with ${valueAllCards} points after you've stop the game</p>
+                <p><div>The last card drawn is the ${arrCard.cards[0].value} of ${arrCard.cards[0].suit}</p>
+                <img id="imgCard + ${deck.remaining}" src="${arrCard.cards[0].image}">
+            `
                 console.log("perdu")
             }
         }
 
         else if (!bool){
             if(valueAllCards === 21){
-                document.getElementById("state").innerHTML = "C'est gagné";
-                document.getElementById("canvas").innerHTML = "<p>C'est gagné</p>"
+
+                // animation(150,100,0,0);
+
+                document.getElementById("state").innerHTML = "We have a winner";
+                document.getElementById("canvas").innerHTML = `
+                <h1>We have a winner</h1> 
+                <p>The total of points have not been over 21 with ${valueAllCards} points</p>
+                <p><div>The last card drawn is the ${arrCard.cards[0].value} of ${arrCard.cards[0].suit}</p>
+                <img id="imgCard + ${deck.remaining}" src="${arrCard.cards[0].image}">
+            `
                 console.log("gagné");
             }
             else if (valueAllCards > 21){
-                document.getElementById("state").innerHTML = "C'est perdu";
-                document.getElementById("canvas").innerHTML = "<p>C'est perdu</p>"
+
+                // animation(0,0,150,100);
+                
+                document.getElementById("state").innerHTML = "It's lost";
+                document.getElementById("canvas").innerHTML = `
+                <h1>It's lost</h1> 
+                <p>The total of points have been over 21 with ${valueAllCards} points</p>
+                <p><div>The last card drawn is the ${arrCard.cards[0].value} of ${arrCard.cards[0].suit}</p>
+                <img id="imgCard + ${deck.remaining}" src="${arrCard.cards[0].image}">
+            `
                 console.log("perdu")
             }
         }
-
-        // document.getElementById("canvas").style.display = "block";
-
     
     }
-    catch (error) {
-      console.log(error);
+    catch (error){
+        document.getElementById("errorCanvas").style.display = "block";
+        console.log(error);
+        document.getElementById("textError").innerHTML += `<div id="errorMessages">${error}</div>`;
     }
-  }
+}
+
+const animation = async (widthAn,widthEnd,heightAn,heightEnd) => {
+    for (let cards = numberCards; cards >= deck.remaining; cards--) {
+        // console.log('stopTheGame: card ' + cards + " deck remaining " + deck.remaining)
+
+        let image = document.getElementById("imgCard" + cards);
+
+        image.animate([
+            {transform: `translate(${widthAn}vw,${heightAn}vh)`}
+        ],
+        {duration: 2000})
+        
+        image.style.transform = `translate(${widthEnd}vw,${heightEnd}vh)`;
+    
+    }
+}
